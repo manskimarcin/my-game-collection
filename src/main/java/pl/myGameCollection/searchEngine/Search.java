c54756fb7f91142bacc9fb066f67bff2710ed7a6
Marcin Manski <marcin.manski1994@gmail.com>
2019-11-07 23:35:39 +0100

    Remove Search.java interface

--- src/main/java/pl/myGameCollection/searchEngine/Search.java
+++
@@ -1,8 +0,0 @@
-package pl.myGameCollection.searchEngine;
-
-public interface Search {
-    final String METACRITIC_URL = "https://www.metacritic.com/";
-    final String HTLTB_URL =  "https://howlongtobeat.com/";
-
-    String getWebsiteURL();
-}
