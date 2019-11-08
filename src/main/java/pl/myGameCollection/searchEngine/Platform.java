8428c280151cb8f0f47fa5cd72df6eee20c7239a
Marcin Manski <marcin.manski1994@gmail.com>
2019-11-08 10:40:42 +0100

    Add Platform.java enum

---
+++ src/main/java/pl/myGameCollection/searchEngine/Platform.java
@@ -0,0 +1,19 @@
+package pl.myGameCollection.searchEngine;
+
+public enum Platform {
+    //SONY
+    PS, P22, PS3, PS4,
+    PSP, PS_VITA,
+
+    //MICROSOFT
+    XBOX, XBOX360, XBOX_ONE,
+
+    //NINTENDO
+    GC, WII, WII_U, SWITCH,
+    GB, GBC, GBA, NDS, N3DS, NEW_N3DS
+
+    //SEGA
+
+    //ATARI
+
+}
