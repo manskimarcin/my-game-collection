039f30a3fb877d36be555ded2f12e8a21989c5be
Marcin Manski <marcin.manski1994@gmail.com>
2019-11-10 01:20:32 +0100

    Fix bug in checkIfGameHasAdditionalContent, add second test to ExtractFromHLTBTest.java

--- src/main/java/pl/myGameCollection/searchEngine/ExtractFromHLTB.java
+++ src/main/java/pl/myGameCollection/searchEngine/ExtractFromHLTB.java
@@ -2,7 +2,6 @@ package pl.myGameCollection.searchEngine;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
@@ -22,16 +21,20 @@ public class ExtractFromHLTB {
         hltbUrl = googleSearch.getWebsiteURL();
         hltbSourceCode = Jsoup.connect(hltbUrl).get();
         this.googleSearch = googleSearch;
+        checkIfGameHasAdditionalContent();
         getTimes();
     }
 
     public void getTimes(){
         Elements times = hltbSourceCode.select("li[class='short time_100']");
-
         this.mainStory = times.get(0).text();
         this.mainPlusExtras = times.get(1).text();
         this.completionist = times.get(2).text();
         this.allStyles = times.get(3).text();
+    }
+
+    public boolean getHasAdditionalContent() {
+        return hasAdditionalContent;
     }
 
     public String getMainStory() {
@@ -50,8 +53,8 @@ public class ExtractFromHLTB {
         return allStyles;
     }
 
-    public void checkIfGameHasAdditionalContent(){
-        this.hasAdditionalContent = hltbSourceCode.select("").text().length() != 0;
-    }
 
+    public void checkIfGameHasAdditionalContent(){
+        this.hasAdditionalContent = hltbSourceCode.select("[class='in scrollable back_primary shadow_box']").text().length() != 0;
+    }
 }
--- src/test/java/pl/myGameCollection/searchEngine/ExtractFromHLTBTest.java
+++ src/test/java/pl/myGameCollection/searchEngine/ExtractFromHLTBTest.java
@@ -9,9 +9,9 @@ import static org.junit.jupiter.api.Assertions.*;
 class ExtractFromHLTBTest {
 
     @Test
-    void testIfSelectorsWorksProperly() throws IOException {
-        String title = "super mario galaxy";
-        String platform = "Wii";
+    void testIfSelectorsWorksProperlyForWitcher3() throws IOException {
+        String title = "witcher 3";
+        String platform = "PS4";
         Website website = Website.HLTB;
         GoogleSearch googleSearch = new GoogleSearch(title, platform, website);
         ExtractFromHLTB extractFromHLTB = new ExtractFromHLTB(googleSearch);
@@ -20,7 +20,22 @@ class ExtractFromHLTBTest {
         System.out.println(extractFromHLTB.getMainPlusExtras());
         System.out.println(extractFromHLTB.getCompletionist());
         System.out.println(extractFromHLTB.getAllStyles());
+        System.out.println(extractFromHLTB.getHasAdditionalContent());
+    }
 
+    @Test
+    void testIfSelectorsWorksProperlyForSuperMarioGalaxy() throws IOException {
+        String title = "Super Mario Galaxy";
+        String platform = "WII";
+        Website website = Website.HLTB;
+        GoogleSearch googleSearch = new GoogleSearch(title, platform, website);
+        ExtractFromHLTB extractFromHLTB = new ExtractFromHLTB(googleSearch);
+
+        System.out.println(extractFromHLTB.getMainStory());
+        System.out.println(extractFromHLTB.getMainPlusExtras());
+        System.out.println(extractFromHLTB.getCompletionist());
+        System.out.println(extractFromHLTB.getAllStyles());
+        System.out.println(extractFromHLTB.getHasAdditionalContent());
     }
 
 }
